33798ca16a2d22700233666cd3abb0c98560b96a
David Moll <dlmoll@proton.me>
2025-04-01 08:57:30 +0200

    Inline styling for valid html

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,15 @@
 module.exports = function (eleventyConfig, options = {}) {
-
-const id = options.id || 'main-content'
-const text = options.text || 'Jump to main content'
+  const id = options.id || 'main-content';
+  const text = options.text || 'Jump to main content';
 
   eleventyConfig.addShortcode("skiplink", function () {
     return `
-        <style>
-          .skip-link {
-              position: absolute;
-              top: -40px;
-              left: 10px;
-              background: #000;
-              color: #fff;
-              padding: 8px;
-              z-index: 100;
-          }
-          .skip-link:focus {
-              top: 10px;
-          }
-        </style>
-        <a href="#${id}" class="skip-link">${text}</a>
-      `;
+      <a href="#${id}" 
+         style="position: absolute; top: -40px; left: 10px; background: #000; color: #fff; padding: 8px; z-index: 100;"
+         onfocus="this.style.top='10px'" 
+         onblur="this.style.top='-40px'">
+        ${text}
+      </a>
+    `;
   });
 };
